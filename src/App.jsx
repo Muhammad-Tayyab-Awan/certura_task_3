9ae43a5d476ff100cb35e42d4415d162310eb896
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-19 10:47:33 +0500

    Updated HTML title and structure; implement routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return <>hello</>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
