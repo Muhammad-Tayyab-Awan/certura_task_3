323a1024be484c7c68654d00167d494dc548dd07
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-24 18:49:05 +0500

    refactored: simplify user authentication context by removing unused imports and variables

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,16 +1,14 @@
 /* eslint-disable react-refresh/only-export-components */
-/* eslint-disable react-hooks/exhaustive-deps */
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 import { createContext, useContext, useEffect, useState } from "react";
-import firebaseApp from "../config/firebase.config";
+import { firebaseAuth } from "../config/firebase.config";
 
 const userContext = createContext({ user: null, setUser: () => {} });
 
 export function UserProvider({ children }) {
   const [user, setUser] = useState({ user: null, setUser: () => {} });
-  const auth = getAuth(firebaseApp);
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    onAuthStateChanged(firebaseAuth, (user) => {
       if (user) {
         setUser(user);
       } else {
