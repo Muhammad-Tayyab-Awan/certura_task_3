d1a239f44fd86c8ccc13f52ecf7d59ace06f3dff
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-25 10:06:57 +0500

    feat: added Footer component for site footer

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="mt-20 bg-gray-300 p-4 text-center text-sm text-gray-500">
+      Made with Love by Muhammad Tayyab | eQurban.com &copy;{" "}
+      {new Date().getFullYear()}
+    </footer>
+  );
+}
+
+export default Footer;
