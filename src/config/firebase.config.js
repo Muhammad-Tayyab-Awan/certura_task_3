d00c69b2df0dcbdedaef24fbc3b288e611e8b44f
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-05-25 10:08:20 +0500

    feat: d Firebase authentication and Firestore database

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -10,4 +12,6 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
+export const fireStoreDb = getFirestore(firebaseApp);
+export const firebaseAuth = getAuth(firebaseApp);
 export default firebaseApp;
